e4866a522c3556815243dfa1e6eb933d381d1ac0
AbHiN45 <108576269+AbHiN45@users.noreply.github.com>
2022-07-16 20:55:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
 
+
+API_ID = "15476227"
+API_HASH = "c64d4881f7d539052942f6f03dfe9d60"
+BOT_TOKEN = "5329764861:AAHfUq6IlAfIRE_e8n1zXNQmBuI06R__QPU
+"
+
+
+ABHI = Client(
+    name="pyrogrambot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("Bot Started")
+
+ABHI.run()
